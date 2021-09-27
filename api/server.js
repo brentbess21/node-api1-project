465aaa5c89d7275b60488f984c07038340228c6e
Brent Besselievre <brent@Brents-MacBook-Pro.local>
2021-09-27 15:01:57 -0600

    DELETE endpoint created. Tests 9-11 are passing

--- api/server.js
+++ api/server.js
@@ -65,4 +65,25 @@ server.post('/api/users', async (req, res)=> {
     }
 })
 
+//DELETE endpoint 
+
+server.delete('/api/users/:id', async (req, res)=> {
+    try {
+        const { id } = req.params;
+        const deletedUser = await User.remove(id)
+        if(!deletedUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        } else {
+            res.status(200).json(deletedUser)
+        }
+    } 
+    catch (err) {
+        res.status(500).json({
+            message: "The user could not be removed"
+        })
+    }
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
