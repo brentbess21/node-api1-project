28b6ea9d697a426263912c0f75800ceb3093849d
Brent Besselievre <brent@Brents-MacBook-Pro.local>
2021-09-27 14:39:07 -0600

    Added Get /api/users/:id endpoint. Passes test 7 and 8

--- api/server.js
+++ api/server.js
@@ -11,6 +11,8 @@ const server = express ()
 server.use(express.json())
 
 //endpoints
+
+// GET endpoints
 server.get('/api/users', async (req, res)=> {
     try {
         const users = await User.find();
@@ -21,6 +23,24 @@ server.get('/api/users', async (req, res)=> {
           message: "The user information could not be retrieved"
       })
 
+    }
+})
+
+server.get('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const user = await User.findById(id)
+        if (!user) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        } else {
+        res.status(200).json(user)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The user information could not be retrieved"
+        })
     }
 })
 
