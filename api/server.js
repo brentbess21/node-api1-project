d77e08acc4a0c95cd4ca39be05a1fd1deee11ec3
Brent Besselievre <brent@Brents-MacBook-Pro.local>
2021-09-27 14:24:33 -0600

    Built Get endpoint for api/users. Passes auto tests 5 and 6

--- api/server.js
+++ api/server.js
@@ -2,11 +2,26 @@
 
 //imports
 const express = require('express');
+const User = require('./users/model');
 
 //instance of express app 
 const server = express ()
 
 //global middleware
 server.use(express.json())
+
+//endpoints
+server.get('/api/users', async (req, res)=> {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+
+      res.status(500).json({
+          message: "The user information could not be retrieved"
+      })
+
+    }
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
